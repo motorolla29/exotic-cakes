40133c7ea1cc9046966cf00709c39e50431fb86e
motorolla29 <eutyou@gmail.com>
2024-09-24 16:53:39 +0300

    home page merch promo block styled

--- src/components/pages/home-page/home-page.jsx
+++ src/components/pages/home-page/home-page.jsx
@@ -6,6 +6,7 @@ import PlayIcon from '../../../icons/play.svg';
 import PauseIcon from '../../../icons/pause.svg';
 
 import './home-page.sass';
+import { Link } from 'react-router-dom';
 
 const HomePage = () => {
   const [videoPlaying, setVideoPlaying] = useState(true);
@@ -34,6 +35,25 @@ const HomePage = () => {
             className="play-icon"
           />
         )}
+      </div>
+      <div className="merch-promo-wrapper">
+        <div className="merch-promo">
+          <img className="merch-promo_image" src="/images/merch-promo.png" />
+          <div className="merch-promo_info">
+            <p className="merch-promo_info_title">MEET OUR MERCH!</p>
+            <p className="merch-promo_info_description">
+              Introducing Exotic Cakes merchandise! Now you can celebrate your
+              love for our one-of-a-kind, artistic cakes with exclusive branded
+              apparel and accessories. From stylish T-shirts to unique tote
+              bags, our merch is as bold and colorful as our signature
+              confections. Show off your passion for extraordinary desserts and
+              stand out with Exotic Cakes!
+            </p>
+            <Link className="merch-promo_info_link" to="/merch">
+              Visit Shop
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
   );

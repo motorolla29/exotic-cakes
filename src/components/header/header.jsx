aa03b9459475bb81b3806b19fb0bcf4a7ac67f27
motorolla29 <eutyou@gmail.com>
2024-09-25 17:08:43 +0300

    header, menu page adapts

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,32 +1,40 @@
 import { NavLink, Link } from 'react-router-dom';
+
+import useWindowSize from '../../hooks/use-window-size';
+
 import CartIcon from '../../icons/cart-icon.svg';
 
 import './header.sass';
 
 const Header = () => {
+  const [ww] = useWindowSize();
+
   return (
     <div className="header">
+      {ww <= 992 ? <div className="header_burger">Burger</div> : null}
       <Link to="/" className="header_logo">
         <img src="/images/EC-logo-8bit.png" alt="logo" />
       </Link>
       <div className="header_right-side">
-        <div className="header_right-side_navbar">
-          <div className="header_right-side_navbar_link">
+        {ww > 992 ? (
+          <div className="header_right-side_navbar">
             <div className="header_right-side_navbar_link">
-              <NavLink to={'/'}>Home</NavLink>
+              <div className="header_right-side_navbar_link">
+                <NavLink to={'/'}>Home</NavLink>
+              </div>
+              <NavLink to={'/menus'}>Menus</NavLink>
             </div>
-            <NavLink to={'/menus'}>Menus</NavLink>
+            <div className="header_right-side_navbar_link">
+              <NavLink to={'/about'}>About</NavLink>
+            </div>
+            <div className="header_right-side_navbar_link">
+              <NavLink to={'/location'}>Location</NavLink>
+            </div>
+            <div className="header_right-side_navbar_link">
+              <NavLink to={'/merch'}>Merch</NavLink>
+            </div>
           </div>
-          <div className="header_right-side_navbar_link">
-            <NavLink to={'/about'}>About</NavLink>
-          </div>
-          <div className="header_right-side_navbar_link">
-            <NavLink to={'/location'}>Location</NavLink>
-          </div>
-          <div className="header_right-side_navbar_link">
-            <NavLink to={'/merch'}>Merch</NavLink>
-          </div>
-        </div>
+        ) : null}
         <Link to="/cart" className="header_right-side_cart-icon">
           <CartIcon />
           <div className="header_right-side_cart-icon_counter">

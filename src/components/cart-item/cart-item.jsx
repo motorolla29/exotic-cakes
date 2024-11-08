50331f727c95d3c39e9b5ca038a7d9b36127ce66
motorolla29 <eutyou@gmail.com>
2024-11-08 11:47:39 +0300

    cancel deletion cart item logic

--- src/components/cart-item/cart-item.jsx
+++ src/components/cart-item/cart-item.jsx
@@ -1,4 +1,4 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { observer } from 'mobx-react-lite';
 import { motion } from 'framer-motion';
@@ -36,6 +36,8 @@ const CartItem = observer(({ item }) => {
   } = item;
   const [ww] = useWindowSize();
   const cartItemRef = useRef();
+  const [deletion, setDeletion] = useState(false);
+  const [deletionTimeout, setDeletionTimeout] = useState(null);
 
   const onQuantityInputHandler = (e) => {
     e.target.value = ~~e.target.value;
@@ -48,7 +50,19 @@ const CartItem = observer(({ item }) => {
   };
 
   const onDeleteBinClick = () => {
-    store.removeItemFromCart(item);
+    setDeletion(true);
+    setDeletionTimeout(
+      setTimeout(() => {
+        store.removeItemFromCart(item);
+      }, 4000)
+    );
+  };
+
+  const onCancelDeletionButtonClick = () => {
+    if (deletionTimeout) {
+      clearTimeout(deletionTimeout);
+    }
+    setDeletion(false);
   };
 
   return (
@@ -57,150 +71,159 @@ const CartItem = observer(({ item }) => {
       exit={cartItemExitAnimationSequence(cartItemRef)}
       className="cart-item"
     >
-      <div className="cart-item_main">
-        <Link
-          to={
-            type === 'merch'
-              ? `/merch/${id}?${stringParams ? stringParams : ''}`
-              : `/menus/${category}/${id}?${stringParams ? stringParams : ''}`
-          }
-          className="cart-item_main_img"
-        >
-          <img
-            src={`${
-              type === 'merch' ? baseMerchImagesURL : baseImagesURL
-            }/${image}`}
-            alt="product-image"
-          />
-        </Link>
-        <div className="cart-item_main_info">
+      <div className={`cart-item-container ${deletion && 'deletion'}`}>
+        <div className="cart-item_main">
           <Link
-            to={type === 'merch' ? `/merch/${id}` : `/menus/${category}/${id}`}
+            to={
+              type === 'merch'
+                ? `/merch/${id}?${stringParams ? stringParams : ''}`
+                : `/menus/${category}/${id}?${stringParams ? stringParams : ''}`
+            }
+            className="cart-item_main_img"
           >
-            <p className="cart-item_main_info_title">{title}</p>
+            <img
+              src={`${
+                type === 'merch' ? baseMerchImagesURL : baseImagesURL
+              }/${image}`}
+              alt="product-image"
+            />
           </Link>
-          {ww <= 768 ? (
-            <div className="cart-item_sm-price-quantity">
-              <span className="cart-item_price">
-                Price:<span>${price}</span>
-              </span>
-              <div className="cart-item_counter">
-                Quantity:
-                <div className="cart-item_counter_input">
-                  <TiMinusOutline
-                    onClick={() => store.decrementItemCountInCart(item)}
-                    className={`${item.quantity < 2 ? 'disabled' : ''}`}
-                  />
-                  <input
-                    defaultValue={item.quantity}
-                    onKeyDown={(e) => {
-                      if (e.key === 'Enter') {
-                        onQuantityInputHandler(e);
-                        e.target.blur();
-                      }
-                    }}
-                    onBlur={onQuantityInputHandler}
-                    type="number"
-                    min={1}
-                    max={99}
-                  />
-                  <TiPlusOutline
-                    onClick={() => store.incrementItemCountInCart(item)}
-                    className={`${item.quantity > 98 ? 'disabled' : ''}`}
-                  />
+          <div className="cart-item_main_info">
+            <Link
+              to={
+                type === 'merch' ? `/merch/${id}` : `/menus/${category}/${id}`
+              }
+            >
+              <p className="cart-item_main_info_title">{title}</p>
+            </Link>
+            {ww <= 768 ? (
+              <div className="cart-item_sm-price-quantity">
+                <span className="cart-item_price">
+                  Price:<span>${price}</span>
+                </span>
+                <div className="cart-item_counter">
+                  Quantity:
+                  <div className="cart-item_counter_input">
+                    <TiMinusOutline
+                      onClick={() => store.decrementItemCountInCart(item)}
+                      className={`${item.quantity < 2 ? 'disabled' : ''}`}
+                    />
+                    <input
+                      defaultValue={item.quantity}
+                      onKeyDown={(e) => {
+                        if (e.key === 'Enter') {
+                          onQuantityInputHandler(e);
+                          e.target.blur();
+                        }
+                      }}
+                      onBlur={onQuantityInputHandler}
+                      type="number"
+                      min={1}
+                      max={99}
+                    />
+                    <TiPlusOutline
+                      onClick={() => store.incrementItemCountInCart(item)}
+                      className={`${item.quantity > 98 ? 'disabled' : ''}`}
+                    />
+                  </div>
                 </div>
               </div>
-            </div>
-          ) : null}
-          {option ? (
-            <p className="cart-item_main_info_options">
-              {`${optionName}: `}
-              <span>{option}</span>
-            </p>
-          ) : null}
-          {spongeVariant ? (
-            <p className="cart-item_main_info_sponge">
-              Sponge: <span>{spongeVariant}</span>
-            </p>
-          ) : null}
-          {fillVariant ? (
-            <p className="cart-item_main_info_filling">
-              Filling Icing: <span>{fillVariant}</span>
-            </p>
-          ) : null}
-          {merchVariants
-            ? Object.entries(merchVariants).map((it) => {
-                return (
-                  <p key={it} className="cart-item_main_info_filling">
-                    {it[0]}: <span>{it[1]}</span>
-                  </p>
-                );
-              })
-            : null}
-          {cakeSign ? (
-            <div className="cart-item_main_info_cake-sign">
-              <span>
-                <LuCake />
-                Write on cake:
-              </span>
-              <p>{cakeSign}</p>
-            </div>
-          ) : null}
-          {cartMessage ? (
-            <div className="cart-item_main_info_card-message">
-              <span>
-                <TbPencilHeart />
-                Card with handwritten text:
-              </span>
-              <p>{cartMessage}</p>
-            </div>
-          ) : null}
-        </div>
-      </div>
-      {ww > 768 ? (
-        <>
-          <p className="cart-item_price">${price}</p>
-          <div className="cart-item_counter">
-            <div className="cart-item_counter_input">
-              <TiMinusOutline
-                onClick={() => store.decrementItemCountInCart(item)}
-                className={`${item.quantity < 2 ? 'disabled' : ''}`}
-              />
-              <input
-                defaultValue={item.quantity}
-                onKeyDown={(e) => {
-                  if (e.key === 'Enter') {
-                    onQuantityInputHandler(e);
-                    e.target.blur();
-                  }
-                }}
-                onBlur={onQuantityInputHandler}
-                type="number"
-                min={1}
-                max={99}
-              />
-              <TiPlusOutline
-                onClick={() => store.incrementItemCountInCart(item)}
-                className={`${item.quantity > 98 ? 'disabled' : ''}`}
-              />
-            </div>
+            ) : null}
+            {option ? (
+              <p className="cart-item_main_info_options">
+                {`${optionName}: `}
+                <span>{option}</span>
+              </p>
+            ) : null}
+            {spongeVariant ? (
+              <p className="cart-item_main_info_sponge">
+                Sponge: <span>{spongeVariant}</span>
+              </p>
+            ) : null}
+            {fillVariant ? (
+              <p className="cart-item_main_info_filling">
+                Filling Icing: <span>{fillVariant}</span>
+              </p>
+            ) : null}
+            {merchVariants
+              ? Object.entries(merchVariants).map((it) => {
+                  return (
+                    <p key={it} className="cart-item_main_info_filling">
+                      {it[0]}: <span>{it[1]}</span>
+                    </p>
+                  );
+                })
+              : null}
+            {cakeSign ? (
+              <div className="cart-item_main_info_cake-sign">
+                <span>
+                  <LuCake />
+                  Write on cake:
+                </span>
+                <p>{cakeSign}</p>
+              </div>
+            ) : null}
+            {cartMessage ? (
+              <div className="cart-item_main_info_card-message">
+                <span>
+                  <TbPencilHeart />
+                  Card with handwritten text:
+                </span>
+                <p>{cartMessage}</p>
+              </div>
+            ) : null}
           </div>
-        </>
-      ) : null}
-      <div className="cart-item_total">
-        <div>
-          ${price * quantity}
-          <RiDeleteBin5Line onClick={onDeleteBinClick} />
+        </div>
+        {ww > 768 ? (
+          <>
+            <p className="cart-item_price">${price}</p>
+            <div className="cart-item_counter">
+              <div className="cart-item_counter_input">
+                <TiMinusOutline
+                  onClick={() => store.decrementItemCountInCart(item)}
+                  className={`${item.quantity < 2 ? 'disabled' : ''}`}
+                />
+                <input
+                  defaultValue={item.quantity}
+                  onKeyDown={(e) => {
+                    if (e.key === 'Enter') {
+                      onQuantityInputHandler(e);
+                      e.target.blur();
+                    }
+                  }}
+                  onBlur={onQuantityInputHandler}
+                  type="number"
+                  min={1}
+                  max={99}
+                />
+                <TiPlusOutline
+                  onClick={() => store.incrementItemCountInCart(item)}
+                  className={`${item.quantity > 98 ? 'disabled' : ''}`}
+                />
+              </div>
+            </div>
+          </>
+        ) : null}
+        <div className="cart-item_total">
+          <div>
+            ${price * quantity}
+            <RiDeleteBin5Line onClick={onDeleteBinClick} />
+          </div>
         </div>
       </div>
-      <div className="cart-item_cancel-delete-button-container">
-        <div className="cart-item_cancel-delete-button">
-          <svg>
-            <rect></rect>
-          </svg>
-          <span>Cancel</span>
+      {deletion && (
+        <div className="cart-item_cancel-delete-button-container">
+          <div
+            onClick={onCancelDeletionButtonClick}
+            className="cart-item_cancel-delete-button"
+          >
+            <svg>
+              <rect></rect>
+            </svg>
+            <span>Cancel</span>
+          </div>
         </div>
-      </div>
+      )}
     </motion.div>
   );
 });

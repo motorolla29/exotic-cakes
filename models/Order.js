d36d447fdf429694d3e91121340f4d37334f32b1
motorolla29 <eutyou@gmail.com>
2025-04-24 11:46:18 +0300

    counter schema fixed

--- models/Order.js
+++ models/Order.js
@@ -1,5 +1,13 @@
 import mongoose from 'mongoose';
 
+const counterSchema = new mongoose.Schema({
+  _id: { type: String, required: true },
+  seq: { type: Number, default: 0 },
+});
+
+const Counter =
+  mongoose.models.Counter || mongoose.model('Counter', counterSchema);
+
 const orderItemSchema = new mongoose.Schema({
   id: { type: String, required: true },
   type: { type: String },
